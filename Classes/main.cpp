feec794fa8b420bdb81ce016a735565d6302051c
FedorsHome1 <nik.goltsev.97@bk.ru>
2023-03-19 02:32:10 +0700

    Second_class

--- Classes/main.cpp
+++ Classes/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 
+// Class number 1, introduction
 class BanckAccount{ // Class as a type to organise some date
     public: // one of two accces options
         // we get acces also from out of a classes
@@ -19,14 +20,52 @@ class BanckAccount{ // Class as a type to organise some date
 };
 
 
+// Class number 2. Acces specefication
+class Employee
+{
+private:
+    /* data */
+    std::string name;
+
+    void set_bill(double mean){
+        if(mean != 0)bill_bm = mean;
+        else bill_bm = 100;
+    }
+
+    double get_result(){
+        double result = -1;
+        std::string your_name;
+        std::cin << your_name;
+        if(your_name == name){
+            result = bill_bm;
+        }
+        return result;
+    }
+
+public:
+    double bill_bm;
+};
+
+
+
 // Main function here to test classes
 int mian(int argc, char *argv[]){
 
+    /* //Class 01
     // Work with class
     BanckAccount acc01; // That's object creation
     acc01.name = "Higerd";
     acc01.balance = 0; 
     std::cout << acc01.balance << "\n";
+    */
 
+    //Class 02
+    Employee empl01;
+
+    //Example 
+    //1)We can get name from class, but we can't just get value of bill
+    //becasu we have no access
+    //2) But we can use functions from public, that going to get var for us
+    // that functions can protect private vars and funcions from something bad
     return 0;
 }
