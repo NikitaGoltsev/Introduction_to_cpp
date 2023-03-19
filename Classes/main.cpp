e96bbe71e09c00e1d5e5c728ea36e23d7927a33d
FedorsHome1 <nik.goltsev.97@bk.ru>
2023-03-19 18:39:26 +0700

    Class 3, member functions

--- Classes/main.cpp
+++ Classes/main.cpp
@@ -2,70 +2,89 @@
 #include <string>
 
 // Class number 1, introduction
-class BanckAccount{ // Class as a type to organise some date
-    public: // one of two accces options
-        // we get acces also from out of a classes
-        // That's vall
-        std::string name;
-        int balance;
-        // Also we can add functions
-        void withdraw(int amount){
-            balance-= amount;
-        }
-        void print(){
-            std::cout << balance << "\n";
-        }
-    private:
-        //Acces only from within
-};
+class BanckAccount { // Class as a type to organise some date
+public:              // one of two accces options
+  // we get acces also from out of a classes
+  // That's vall
+  std::string name;
+  int balance;
+  // Also we can add functions
+  void withdraw(int amount) { balance -= amount; }
+  void print() { std::cout << balance << "\n"; }
 
+private:
+  // Acces only from within
+};
 
 // Class number 2. Acces specefication
-class Employee
-{
+class Employee {
 private:
-    /* data */
-    std::string name;
+  /* data */
+  std::string name;
 
-    void set_bill(double mean){
-        if(mean != 0)bill_bm = mean;
-        else bill_bm = 100;
-    }
+  void set_bill(double mean) {
+    if (mean != 0)
+      bill_bm = mean;
+    else
+      bill_bm = 100;
+  }
 
-    double get_result(){
-        double result = -1;
-        std::string your_name;
-        std::cin << your_name;
-        if(your_name == name){
-            result = bill_bm;
-        }
-        return result;
+  double get_result() {
+    double result = -1;
+    std::string your_name;
+    std::cin >> your_name;
+    if (your_name == name) {
+      result = bill_bm;
     }
+    return result;
+  }
 
 public:
-    double bill_bm;
+  double bill_bm;
 };
 
+// class 3 member functions
+class Rectangle {
+private:
+  /* data */
+  double length;
+  double width;
 
+public:
+  void set_dmnsions(double l, double w) { // That's member function of rectangle
+    length = l;
+    width = w;
+  } // Here we disided to define the function from within of class body
+
+  // Also we can define the header of function here and write it outside of the
+  // class
+  double perimeter();
+};
+
+// Link to the function from class
+double Rectangle::perimeter() { return 2 * (length + width); }
 
 // Main function here to test classes
-int mian(int argc, char *argv[]){
+int mian(int argc, char *argv[]) {
 
-    /* //Class 01
-    // Work with class
-    BanckAccount acc01; // That's object creation
-    acc01.name = "Higerd";
-    acc01.balance = 0; 
-    std::cout << acc01.balance << "\n";
-    */
+  /* //Class 01
+  // Work with class
+  BanckAccount acc01; // That's object creation
+  acc01.name = "Higerd";
+  acc01.balance = 0;
+  std::cout << acc01.balance << "\n";
+  */
 
-    //Class 02
-    Employee empl01;
+  // Class 02
+  // Employee empl01;
 
-    //Example 
-    //1)We can get name from class, but we can't just get value of bill
-    //becasu we have no access
-    //2) But we can use functions from public, that going to get var for us
-    // that functions can protect private vars and funcions from something bad
-    return 0;
+  // Example
+  // 1)We can get name from class, but we can't just get value of bill
+  // becasu we have no access
+  // 2) But we can use functions from public, that going to get var for us
+  //  that functions can protect private vars and funcions from something bad
+
+  // Class 03
+
+  return 0;
 }
