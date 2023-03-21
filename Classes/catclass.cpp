2c8a78cd16209dbbc49b03bbc453a8d769c9a80e
FedorsHome1 <nik.goltsev.97@bk.ru>
2023-03-22 01:54:33 +0700

    avg_save

--- Classes/catclass.cpp
+++ Classes/catclass.cpp
@@ -1,4 +1,19 @@
 #include <iostream>
+#include <string>
+
+// How to create constructers and destructors
+class Cat{
+    private:
+        std::string name;
+        std::string color;
+        std::string cats_toy;
+    public:
+        void print(){
+            std::cout << "" << name << "\n";
+            std::cout << "" << color << "\n";
+            std::cout << "" << cats_toy << "\n";
+        }
+};
 
 int main(){
 
