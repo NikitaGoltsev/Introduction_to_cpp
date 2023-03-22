2935ebe2533e38a288f8d79326bc32c6aadaf92b
FedorsHome1 <nik.goltsev.97@bk.ru>
2023-03-22 22:32:54 +0700

    Constructor_and_destructor

--- Classes/catclass.cpp
+++ Classes/catclass.cpp
@@ -4,14 +4,26 @@
 // How to create constructers and destructors
 class Cat{
     private:
+        int *countr;
         std::string name;
         std::string color;
         std::string cats_toy;
     public:
+        Cat(){
+            name = "Cat";
+            color = "Cat's";
+            cats_toy = "Avg_cat's toy";
+            countr = new int[5];
+        }
         void print(){
             std::cout << "" << name << "\n";
             std::cout << "" << color << "\n";
             std::cout << "" << cats_toy << "\n";
+        }
+
+        ~Cat(){
+            std::cout << "All is ok" << "\n";
+            delete[] countr;
         }
 };
 
@@ -30,7 +42,7 @@ Cat(){
 int main(){
 
     Cat obj1;
-    //obj1.print();
+    obj1.print();
     
     return 0;
 }
